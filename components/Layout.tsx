d2c5cf241d99e866f1bc35b405f27cac2da28e39
Kiichi Tachibana <bodega1123@gmail.com>
2021-01-16 16:42:38 +0900

    Add Layout

---
+++ components/Layout.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+type Props = {
+  title: string;
+};
+
+const Layout: React.FC<Props> = ({ children, title = 'Default title' }) => {
+  return (
+    <div className='flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800'>
+      <Head>{title}</Head>
+      <main className='flex flex-1 justify-center items-center w-screen flex-col'>
+        {children}
+      </main>
+      <footer className='w-full h-6 flex justify-center items-center text-gray-500 text-sm'>
+        &copy;ossan-engineer 2021
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
