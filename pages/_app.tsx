9c907e4429ba360ab68cf9266e18b7061d439df3
Kiichi Tachibana <bodega1123@gmail.com>
2021-01-16 16:42:23 +0900

    Use baseUrl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
 
-import '../styles/globals.css';
+import 'styles/globals.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
